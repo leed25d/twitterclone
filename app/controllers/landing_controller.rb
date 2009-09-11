92da879b8c311f1e298faf6b16f7b471b108e886
lee doolan <lee@stjohn.dsl.snfc21.pacbell.net>
2009-09-11 10:51:38 -0700

    user can sign up or log in on the landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,8 @@
 class LandingController < ApplicationController
-  def index
-  end
-
+   def index  
+     @users = User.all()  
+     respond_to do |format|  
+       format.html  
+     end  
+   end  
 end
