7bc230c64e4098ffcdb550a9bda44a3587162638
lee doolan <lee@stjohn.dsl.snfc21.pacbell.net>
2009-09-12 14:53:52 -0700

    list the logged in users tweets on the landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,8 +1,10 @@
 class LandingController < ApplicationController
-   def index  
-     @users = User.all()  
-     respond_to do |format|  
-       format.html  
-     end  
-   end  
+  def index
+    if session[:user_id]
+      @tweets= Tweet.find(:all, :conditions => "user_id = #{session[:user_id]}", :order => "updated_at DESC", :limit => 20);
+    end
+    respond_to do |format|
+      format.html
+    end
+  end
 end
